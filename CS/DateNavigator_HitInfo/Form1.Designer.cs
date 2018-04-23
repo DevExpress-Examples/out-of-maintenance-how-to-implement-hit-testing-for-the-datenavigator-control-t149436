aa6d2b07e41f6ca74c8055295d89c70ce44e8f56
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DateNavigator_HitInfo/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace DateNavigator_HitInfo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateNavigator1 = new DevExpress.XtraScheduler.DateNavigator();
+            ((System.ComponentModel.ISupportInitialize)(this.dateNavigator1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dateNavigator1
+            // 
+            this.dateNavigator1.DateTime = new System.DateTime(2014, 9, 10, 0, 0, 0, 0);
+            this.dateNavigator1.Location = new System.Drawing.Point(12, 12);
+            this.dateNavigator1.Name = "dateNavigator1";
+            this.dateNavigator1.CellPadding = new System.Windows.Forms.Padding(2);
+            this.dateNavigator1.Size = new System.Drawing.Size(388, 334);
+            this.dateNavigator1.TabIndex = 0;
+            this.dateNavigator1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.dateNavigator1_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 409);
+            this.Controls.Add(this.dateNavigator1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dateNavigator1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraScheduler.DateNavigator dateNavigator1;
+    }
+}
+
